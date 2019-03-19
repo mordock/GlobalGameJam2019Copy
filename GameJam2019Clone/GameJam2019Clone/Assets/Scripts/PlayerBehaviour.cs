e7235c7c56eb8c33566f16c21f7251d2ad5c0341
DESKTOP-2CCSS00\Max <maxweijers@hotmail.nl>
2019-03-19 18:04:12 +0100

    bunch of shit

--- GameJam2019Clone/GameJam2019Clone/Assets/Scripts/PlayerBehaviour.cs
+++ GameJam2019Clone/GameJam2019Clone/Assets/Scripts/PlayerBehaviour.cs
@@ -3,37 +3,76 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour {
-    public float speed = 20;
     private new Rigidbody2D rigidbody2D;
+    new Animation animation;
+    private AudioSource audioSource;
 
     public Animator animator;
 
     public bool right = false;
 
-    new Animation animation;
-	// Use this for initialization
+    public bool canPickUpSmallPot = false;
+    public bool canPickUpMediumPot = false;
+    public bool canPickUpLargePot = false;
+
+    public bool hasSmallPot = false;
+    public bool hasMediumPot = false;
+    public bool hasLargePot = false;
+
+    public float speed = 20;
+    public float potRechargeTime = 5f;
+
+    [Header("Sounds")]
+    public AudioClip moving10;
+
+    [Header("Pots")]
+    public GameObject smallPot;
+    public GameObject mediumPot;
+    public GameObject largePot;
+
 	void Start () {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
         animator = GetComponent<Animator>();
         animation = this.gameObject.GetComponent<Animation>();
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+
+        findPots();
+    }
+
+    void FixedUpdate () {
+        //movement
         Vector2 moveSpeed = new Vector2(Input.GetAxis("Horizontal") * speed * Time.deltaTime, Input.GetAxis("Vertical") * speed * Time.deltaTime);
         rigidbody2D.MovePosition(rigidbody2D.position + moveSpeed);
 
+        //standing still
         if(Input.anyKey == false) {
             animation.Stop("Walking");
             if (right) {
                 transform.localScale = new Vector3(-1, 1, 1);
                 animator.Play("Idle");
+                audioSource.Stop();
             } else {
                transform.localScale = new Vector3(1, 1, 1);
                 animator.Play("Idle");
+                audioSource.Stop();
             }
+
+            findPots();
         }
 
+        //separate sound input because of GetKey
+        if (Input.GetKeyDown(KeyCode.A)) {
+            audioSource.clip = moving10;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+        if (Input.GetKeyDown(KeyCode.D)) {
+            audioSource.clip = moving10;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+
+        //walking animation
         if (Input.GetKey(KeyCode.A)) {
             transform.localScale = new Vector3(1, 1, 1);
             animator.Play("Walking");
@@ -45,5 +84,51 @@ public class PlayerBehaviour : MonoBehaviour {
             animator.Play("Walking");
             right = true;
         }
+        //pick up pots
+        if (Input.GetKeyDown(KeyCode.F)) {
+            if(canPickUpSmallPot){
+                Destroy(smallPot);
+                hasSmallPot = true;
+                SpawnPots.spawnSmallPot = true;
+                SpawnPots.smallTime = potRechargeTime;
+            }
+            if (canPickUpMediumPot) {
+                Destroy(mediumPot);
+                hasMediumPot = true;
+                SpawnPots.spawnMediumPot = true;
+                SpawnPots.mediumTime = potRechargeTime;
+            }
+            if (canPickUpLargePot) {
+                Destroy(largePot);
+                hasLargePot = true;
+                SpawnPots.spawnLargePot = true;
+                SpawnPots.largeTime = potRechargeTime;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("SmallPot")) {
+            canPickUpSmallPot = true;
+        }
+        if (collision.CompareTag("MediumPot")) {
+            canPickUpMediumPot = true;
+        }
+        if (collision.CompareTag("LargePot")) {
+            canPickUpLargePot = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        canPickUpSmallPot = false;
+        canPickUpMediumPot = false;
+        canPickUpLargePot = false;
+    }
+
+    //add a pot to the player if one excists
+    void findPots() {
+        smallPot = GameObject.Find("SmallPotPrefab");
+        mediumPot = GameObject.Find("MediumPotPrefab");
+        largePot = GameObject.Find("LargePotPrefab");
     }
 }
--- GameJam2019Clone/GameJam2019Clone/Assets/SpawnPots.cs
+++ GameJam2019Clone/GameJam2019Clone/Assets/SpawnPots.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class SpawnPots : MonoBehaviour {
 
     public GameObject smallPot, mediumPot, LargePot;
+    GameObject clone;
 
     bool smallPotExists, MediumPotExists, LargePotExists;
+
+    public static bool spawnSmallPot = true, spawnMediumPot = true, spawnLargePot = true;
+
+    public static float smallTime = 5f, mediumTime = 5f, largeTime = 5f;
 
     // Use this for initialization
     void Start () {
@@ -15,9 +20,35 @@ public class SpawnPots : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (!smallPotExists) {
-            Instantiate(smallPot, new Vector3(-1.3f, 0.65f, -1f), Quaternion.identity);
-            smallPotExists = true;
+        smallTime -= Time.deltaTime;
+        mediumTime -= Time.deltaTime;
+        largeTime -= Time.deltaTime;
+
+        if (spawnSmallPot) {
+            if(smallTime <= 0){
+                clone = (GameObject) Instantiate(smallPot, new Vector3(19f,  -1.3f, -2f), Quaternion.identity);
+                clone.name = smallPot.name;
+                spawnSmallPot = false;
+                //smallTime = rechargeTime;
+            }
+        }
+
+        if (spawnMediumPot) {
+            if (mediumTime <= 0f) {
+                clone = (GameObject)Instantiate(mediumPot, new Vector3(19f, -5f, -2f), Quaternion.identity);
+                clone.name = mediumPot.name;
+                spawnMediumPot = false;
+                //mediumTime = rechargeTime;
+            }
+        }
+
+        if (spawnLargePot) {
+            if(largeTime <= 0f) {
+                clone = (GameObject)Instantiate(LargePot, new Vector3(19f, -10f, -2f), Quaternion.identity);
+                clone.name = LargePot.name;
+                spawnLargePot = false;
+                //largeTime = rechargeTime;
+            }
         }
 	}
 }
