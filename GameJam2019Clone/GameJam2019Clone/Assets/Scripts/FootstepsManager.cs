538481e210ba1cf763956681544ca3ea58fbd52e
Pat Cleaver <pat.cleaver.bass@gmail.com>
2019-06-26 18:22:09 +0200

    added "footsteps manager" script to Player, that much seems to work added some parameters to the FMOD footsteps event still have to figure out how switch those parameters based on triggers

---
+++ GameJam2019Clone/GameJam2019Clone/Assets/Scripts/FootstepsManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepsManager : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string inputsound;
+    bool playerismoving;
+    public float walkingspeed;
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
+        {
+            playerismoving = true;
+        }
+        else
+        {
+            playerismoving = false;
+        }
+    }
+
+        void CallFootsteps ()
+    {
+        if (playerismoving == true)
+        {
+            FMODUnity.RuntimeManager.PlayOneShot (inputsound);
+        }
+    }
+
+    private void Start()
+    {
+        InvokeRepeating("CallFootsteps", 0, walkingspeed);
+    }
+    private void OnDisable()
+    {
+        playerismoving = false;
+    }
+}
