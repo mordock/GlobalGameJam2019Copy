e78e543cb0f04217478c7fd3bb030fd5fb90f7f2
Pat Cleaver <pat.cleaver.bass@gmail.com>
2020-03-11 13:42:07 +0100

    -designed the end cutscene sound -fixed a couple events -balance the mix a bit more in general -might be finished...?

---
+++ GameJam2019Clone/GameJam2019Clone/Assets/Scripts/EndWaterSounds.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndWaterSounds : MonoBehaviour
+{
+    FMOD.Studio.EventInstance Water;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Water = FMODUnity.RuntimeManager.CreateInstance("event:/End Water");
+        Water.start();
+        Water.release();
+        
+    }
+
+    private void OnDestroy()
+    {
+        Water.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    }
+}
